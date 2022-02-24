daccb7173e77a49acbbe1b803b222c8c4228f3de
hankc <hankc@umich.edu>
2022-02-23 20:43:23 -0500

    232

---
+++ StacksQueues/232_Implement_Queue_Using_Stacks/sol.cpp
@@ -0,0 +1,46 @@
+class MyQueue {
+private:
+    stack<int> stIn;
+    stack<int> stOut;
+public:
+    MyQueue() {
+
+    }
+    
+    void push(int x) {
+        stIn.push(x);
+    }
+    
+    int pop() {
+        if(stOut.empty()) {
+            while(!stIn.empty()) {
+                stOut.push(stIn.top());
+                stIn.pop();
+            }
+        }
+        
+            int result = stOut.top();
+            stOut.pop();
+            return result;
+        
+    }
+    
+    int peek() {
+        int result = pop();
+        stOut.push(result);
+        return result;
+    }
+    
+    bool empty() {
+        return stIn.empty() && stOut.empty();
+    }
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
